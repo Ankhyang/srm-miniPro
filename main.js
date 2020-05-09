73309d05a1ac896c516e5c88343f639361d2e7fa
yangh <yangh@j-cloud.cn>
2020-05-09 15:39:49 +0800

    项目初始化

--- main.js
+++ main.js
@@ -3,17 +3,12 @@ import App from './App'
 
 // import pageHead from './components/page-head.vue'
 // import pageFoot from './components/page-foot.vue'
-// import uLink from '@/components/uLink.vue'
-// import store from './store'
+
+import store from './store'
 
 Vue.config.productionTip = false
 
-// Vue.prototype.$store = store
-// Vue.prototype.$backgroundAudioData = {
-// 	playing: false,
-// 	playTime: 0,
-// 	formatedPlayTime: '00:00:00'
-// }
+Vue.prototype.$store = store
 
 // Vue.component('page-head', pageHead)
 // Vue.component('page-foot', pageFoot)
@@ -22,7 +17,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-	// store,
+	store,
 	...App
 })
 app.$mount()
