5a99cd275bfbecb468f820846596f670594dd40e
yangh <yangh@j-cloud.cn>
2020-05-11 21:47:32 +0800

    登录界面设计2-波浪

--- pages/component/login/login.vue
+++ pages/component/login/login.vue
@@ -4,7 +4,17 @@
 			<image src="../../../static/logo.png" mode=""></image>
 		</view>
 		<view class="uni-flex uni-row form">
-			<view class="flex-item form-input">
+			
+		</view>
+		<view class="button">
+			<button>登录</button>
+		</view>
+		<view class="wave-wrapper">
+			<i class="wave wave1"></i>
+			<i class="wave wave2"></i>
+		</view>
+		<!-- <view class="uni-flex uni-row form"> -->
+			<!-- <view class="flex-item form-input">
 				<text>用户名</text>
 				<input type="text" placeholder="请输入用户名">
 			</view>
@@ -12,11 +22,11 @@
 			<view class="flex-item form-input">
 				<text>密码</text>
 				<input type="text" password placeholder="请输入密码">
-			</view>
-		</view>
-		<view class="button">
+			</view> -->
+		<!-- </view> -->
+		<!-- <view class="button">
 			<button>登录</button>
-		</view>
+		</view> -->
 	</view>
 </template>
 
@@ -25,68 +35,98 @@
 
 <style scoped>
 	.main{
-		background-color: #46b3e6;
-		height: 100%;
 		width: 100%;
-		display: flex;
-		align-items: center;
-		align-content: center;
-		flex-direction: column;
-		justify-content: center;
+		height: 100%;
+		position: relative;
+		background-color: #e6f8f9;
 	}
 	.logo{
-		margin: 20rpx 0;
+		width: 100%;
+		height: 30%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: #08AEEA;
+		background-image: linear-gradient(30deg,#08AEEA 0%,#7BCDFD 100%);
+		background-image: -webkit-linear-gradient(30deg,#08AEEA 0%,#7BCDFD 100%);
+		background-image: -moz-linear-gradient(30deg,#08AEEA 0%,#7BCDFD 100%);
+		background-image: -o-linear-gradient(30deg,#08AEEA 0%,#7BCDFD 100%);
 	}
 	.logo image{
 		width: 332rpx;
 		height: 113rpx;
 	}
 	.form{
-		width: 90%;
-		height: 370rpx;
-		border-radius: 10rpx;
+		width: 94%;
+		height: 45%;
+		margin: 0 auto;
+		position: absolute;
+		z-index: 2;
+		left: 3%;
+		top:25%;
+		border-radius: 20rpx;
 		background-color: #fff;
-		font-size: 30rpx;
-		align-items: center;
-		align-content: stretch;
-		margin: 30rpx 0;
-		color: #666;
-	}
-	.form .form-input{
-		margin: 30rpx 0;
-	}
-	.form-input text, .form-input input{
-		padding: 20rpx 0 16rpx 18rpx;
-	}
-	::-webkit-input-placeholder { /* WebKit, Blink, Edge */
-	    font-size: 1rem;
-	    color: #ccc;
-	}
-	:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
-	    font-size: 1rem;
-	    color: #ccc;
-	}
-	::-moz-placeholder { /* Mozilla Firefox 19+ */
-	    font-size: 1rem;
-	    color: #ccc;
-	}
-	:-ms-input-placeholder { /* Internet Explorer 10-11 */
-	    font-size: 1rem;
-	    color: #ccc;
-	}
-	.line{
-		width: 100%;
-		height: 2rpx;
-		border-bottom: 2rpx solid #ccc;
+		-moz-box-shadow:0px 3px 9px #ccc; 
+		-webkit-box-shadow:0px 3px 9px #ccc;
+		box-shadow:0px 3px 9px #ccc;
 	}
 	.button{
-		width: 90%;
+		position: absolute;
+		left: 15%;
+		top: 67%;
+		z-index: 3;
+		width: 70%;
+		height: 64rpx;
 	}
-	button{
-		background-color: #f39422;
+	.button button{
 		color: #fff;
-		height: 80rpx;
-		line-height: 80rpx;
-		font-size: 36rpx;
+		font-size: 32rpx;
+		border-radius: 70rpx;
+		background-color: #08AEEA;
+		background-image: linear-gradient(60deg,#08AEEA 0%,#A1F8F9 100%);
+		background-image: -webkit-linear-gradient(60deg,#08AEEA 0%,#A1F8F9 100%);
+		background-image: -moz-linear-gradient(60deg,#08AEEA 0%,#A1F8F9 100%);
+		background-image: -o-linear-gradient(60deg,#08AEEA 0%,#A1F8F9 100%);
+	}
+	.wave-wrapper{
+		width: 100%;
+		height: 80px;
+		position: absolute;
+		bottom: -5%;
+	}
+	.wave {
+		width: 100%;
+		height: 60px;
+		position: absolute;
+	}
+	.wave1 {
+		bottom: 20px;
+		background: url(../../../static/wave_01.png);
+		background-position: 0 0;
+		opacity: 0.4;
+		animation: moveLeft 15s linear infinite;
+	}
+	.wave2 {
+		bottom: 10px;
+		background: url(../../../static/wave_02.png);
+		background-position: 0 0;
+		opacity: 0.6;
+		animation: moveRight 18s linear infinite;
+	}
+	@keyframes moveLeft {
+		0% {
+			background-position-x: 0;
+		}
+		100% {
+			background-position-x: 1920px;
+		}
+	}
+	@keyframes moveRight {
+		0% {
+			background-position-x: 0;
+		}
+		100% {
+			background-position-x: -1920px;
+		}
 	}
 </style>
